a8975e76c8951403e30cd10a359e1df3753245a0
MiguelFPP <84242304+MiguelFPP@users.noreply.github.com>
2022-08-09 18:47:28 -0500

    scafollding

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditStudentsComponent } from './components/add-edit-students/add-edit-students.component';
+import { ListStudentsComponent } from './components/list-students/list-students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListStudentsComponent },
+  { path: 'add', component: AddEditStudentsComponent },
+  { path: 'edit/:id', component: AddEditStudentsComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddEditStudentsComponent } from './components/add-edit-students/add-edit-students.component';
+import { ListStudentsComponent } from './components/list-students/list-students.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditStudentsComponent,
+    ListStudentsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
